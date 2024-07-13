107706faa722698aedd3536fbb396297e179d606
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-07-13 20:24:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,38 @@ import { config } from 'dotenv'
 import { initiateApp } from './src/initiate-app.js'
 config({ path: './config/dev.config.env' })
 import cors from 'cors'
+import "dotenv/config";
+import { Client, GatewayIntentBits, ActivityType } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    status: "idle",
+    activities: [
+      {
+        name: "Tayloor",
+        type: ActivityType.Watching, // WATCHING
+      },
+    ],
+  });
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content.toLowerCase() === "link") {
+    message.channel.send(`https://discord.gg/tayloor`);
+  }
+});
+
+client.login(process.env.TOKEN);
 
 
 
