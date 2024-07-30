3592f4bd9b9c30ba184fa56a7a5b91869362762a
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-07-30 04:43:34 +0300

    Update initiate-app.js

--- src/initiate-app.js
+++ src/initiate-app.js
@@ -1,6 +1,8 @@
 import db_connection from "../DB/connection.js";
 import { globalResponse } from "./middlewares/globalResponse.js";
 import Postrouter from "./modules/Post/post.routes.js";
+import { downloadVideo } from "tiktok-api-downloader";
+
 import router from "./modules/User/user.routes.js";
 export const initiateApp = (app, express) => {
   const port = process.env.PORT;
@@ -13,6 +15,25 @@ export const initiateApp = (app, express) => {
   db_connection();
 
   app.get("/", (req, res) => res.json("games-corner API by Mahmoud Sayed"));
+  app.get("/download", async (req, res) => {
+  const url = req.query.url;
+
+  if (!url) {
+    return res.status(400).json({ status: false, error: "URL is required" });
+  }
+
+  try {
+    let response = await downloadVideo(url);
+    return res.status(200).json(response);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      status: false,
+      error: "Failed to download video",
+    });
+  }
+});
+
   app.listen(port, () =>
     console.log(`games-corner listening on port ${port}!`)
   );
